f974efb6dbb87f8a28575c445157eb452bdfb40e
Dimanchella <dimuka0901@gmail.com>
2023-12-01 02:51:17 +0300

    add text formatter

---
+++ src/main/java/ru/mai/arachni/teacher/datasetcreator/TextFormatter.java
@@ -0,0 +1,40 @@
+package ru.mai.arachni.teacher.datasetcreator;
+
+import company.evo.jmorphy2.MorphAnalyzer;
+import company.evo.jmorphy2.ResourceFileLoader;
+
+import java.io.IOException;
+import java.util.List;
+
+public class TextFormatter {
+    private static final String LANG = "ru";
+    private static final String PATH = String.format("/company/evo/jmorphy2/%s/pymorphy2_dicts", LANG);
+
+    private final MorphAnalyzer morphy;
+
+    public TextFormatter() throws IOException {
+        morphy = newMorphAnalyzer();
+    }
+
+    private static MorphAnalyzer newMorphAnalyzer()
+            throws IOException {
+        return new MorphAnalyzer.Builder()
+                .fileLoader(new ResourceFileLoader(PATH))
+                .charSubstitutes(null)
+                .build();
+    }
+
+    public List<String> getNormalForm(String word) throws IOException {
+        return morphy.normalForms(word);
+    }
+
+    public String formatText(String text) throws IOException {
+        String[] words = text
+                .replaceAll("[.,;:'\"()?!]", "")
+                .split("\\s");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = getNormalForm(words[i]).get(0);
+        }
+        return String.join(" ", words);
+    }
+}
